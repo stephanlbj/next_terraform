25976c405457c4c741b665ad6b71a201e95683cd
stephanjames <stephanjames055@gmail.com>
2025-08-29 22:40:23 +0000

    uodated the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="text-red-500 text-3xl ">
-      Intro to terraform + github actions + aws (IAM)
-    </h1>
+    <>
+      <h1>Best app</h1>
+      <h1 className="text-red-500 text-3xl ">
+        Intro to terraform + github actions + aws (IAM)
+      </h1>
+    </>
   );
 }
