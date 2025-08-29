6eb60bf6eb5be2cd78cc09772a0b1fcb997ca0a4
stephanjames <stephanjames055@gmail.com>
2025-08-29 22:53:54 +0000

    added 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Best app</h1>
+      <h1>Best app 2</h1>
       <h1 className="text-red-500 text-3xl ">
         Intro to terraform + github actions + aws (IAM)
       </h1>
