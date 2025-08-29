b6e2b9e5797238d780a0d05f694980f3f2ae02fb
stephanjames <stephanjames055@gmail.com>
2025-08-29 22:23:00 +0000

    fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-
 export default function Home() {
   return (
-    <h1 className="text-red-500 text-3xl ">Intro to terraform + github actions +  aws (IAM) bb  </h1>
+    <h1 className="text-red-500 text-3xl ">
+      Intro to terraform + github actions + aws (IAM)
+    </h1>
   );
 }
